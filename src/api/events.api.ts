c67b50e4dd55981e56f56d70b18bb2fc2bf1eb1c
Maulen <azikulovv@gmail.com>
2026-05-11 22:39:54 +0500

    feat: add event types and update event API functions; improve type safety and structure

--- src/api/events.api.ts
+++ src/api/events.api.ts
@@ -1,52 +1,42 @@
 import { api } from './client'
+import { EventGameStatus } from '../types/api/event'
+import type {
+  Event,
+  GetEventsParams,
+  GetEventsResponse,
+  RegisterToEventPayload,
+  RegisterToEventResponse,
+  RegistrationCheckEventParams,
+  UnregisterFromEventPayload,
+  UnregisterFromEventResponse,
+} from '../types/api/event'
 
-export type Event = {
-  id: string
-  imageUrl: string
-  imageHash: string
-  address: string
-  gameType: string
-  startsAt: string
-  endsAt: null
-  participantLimit: number
-  pointsForParticipation: number
-  status: string
-  createdAt: string
-  updatedAt: string
-  _count: { registrations: number }
+export async function getEvents(params?: GetEventsParams): Promise<Event[]> {
+  const { data } = await api.get<GetEventsResponse>('/events', {
+    params: { status: EventGameStatus.published, ...params },
+  })
+
+  return data.data
 }
 
-type GetEventsResponse = {
-  data: Event[]
+export async function registrationCheckEvent(params: RegistrationCheckEventParams) {
+  return api.get(`/bot/events/${params.eventId}/registration`, {
+    params: { telegramUserId: params.telegramUserId },
+  })
 }
 
-export async function getEvents(game?: string): Promise<Event[]> {
-  const response = await api.get<GetEventsResponse>('/events', {
-    params: {
-      gameType: game,
-      status: 'published',
+export async function registerToEvent(payload: RegisterToEventPayload) {
+  return api.post<RegisterToEventResponse, RegisterToEventPayload>(
+    `/bot/events/${payload.eventId}/register`,
+    payload,
+  )
+}
+
+export async function unregisterFromEvent(payload: UnregisterFromEventPayload) {
+  return api.delete<UnregisterFromEventResponse, UnregisterFromEventPayload>(
+    `/bot/events/${payload.eventId}/register`,
+    {
+      data: payload,
     },
-  })
-
-  return response.data.data
-}
-
-export async function registerToEvent(eventId: string, userId: string) {
-  return api.post(`/bot/events/${eventId}/register`, {
-    eventId: eventId,
-    telegramUserId: userId,
-  })
-}
-
-export async function unregisterFromEvent(eventId: string, userId: string) {
-  return api.delete(`/bot/events/${eventId}/register`, {
-    data: {
-      telegramUserId: userId,
-      eventId: eventId,
-    },
-  })
-}
-
-export async function registrationCheckEvent(eventId: string, userId: string) {
-  return api.get(`/bot/events/${eventId}/registration`, { params: { telegramUserId: userId } })
+  )
 }
---
+++ src/types/api/event.ts
@@ -0,0 +1,60 @@
+export const EventGameType = {
+  billiards: 'billiards',
+  mafia: 'mafia',
+  quiz: 'quiz',
+  poker: 'poker',
+  darts: 'darts',
+} as const
+
+export type EventGameType = (typeof EventGameType)[keyof typeof EventGameType]
+
+export const EventGameStatus = {
+  published: 'published',
+  cancelled: 'cancelled',
+  draft: 'draft',
+} as const
+
+export type EventGameStatus = (typeof EventGameStatus)[keyof typeof EventGameStatus]
+
+export type Event = {
+  id: string
+  city: string
+  endsAt: null
+  status: EventGameStatus
+  address: string
+  features: string
+  gameType: EventGameType
+  gameRules: string
+  startsAt: string
+  imageUrl: string
+  imageHash: string
+  participantLimit: number
+  _count: { registrations: number }
+}
+
+/**
+ *  API types
+ */
+export type GetEventsParams = Partial<{
+  status: EventGameStatus
+  gameType: EventGameType
+}>
+
+export type GetEventsResponse = {
+  data: Event[]
+}
+
+export type RegistrationCheckEventParams = {
+  eventId: string
+  telegramUserId: string
+}
+
+export type RegistrationCheckEventResponse = {}
+
+export type RegisterToEventPayload = RegistrationCheckEventParams
+
+export type RegisterToEventResponse = {}
+
+export type UnregisterFromEventPayload = RegistrationCheckEventParams
+
+export type UnregisterFromEventResponse = {}
