eb32f0d6b195a41633f35917a4c524a1dacce4d1
Maulen <azikulovv@gmail.com>
2026-06-05 23:20:32 +0500

    feat: добавлена картинка в ответ обработчика start; обновлена стартовая картинка

--- src/handlers/start.handler.ts
+++ src/handlers/start.handler.ts
@@ -1,9 +1,10 @@
-import { Bot } from 'grammy'
+import { Bot, InputFile } from 'grammy'
 import { BotContext } from '../types/context'
 import { openAppKeyboard } from '../keyboards/common.keyboard'
 import { trackTelegramPromoStart } from '../api/promo-links.api'
 import { extractStartPayload, isValidPromoCode } from '../utils/promo'
 import { START_MESSAGE } from '../constants/messages'
+import path from 'path'
 
 export function registerStartHandler(bot: Bot<BotContext>) {
   bot.command('start', async (ctx) => {
@@ -28,7 +29,8 @@ export function registerStartHandler(bot: Bot<BotContext>) {
       }
     }
 
-    await ctx.reply(START_MESSAGE, {
+    await ctx.replyWithPhoto(new InputFile(path.resolve('assets/start.jpg')), {
+      caption: START_MESSAGE,
       reply_markup: openAppKeyboard(promoCode),
     })
   })
